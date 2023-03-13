71cdb46921e22362193b86582ec1a9089adfee36
NatShiv <happy_fox@bk.ru>
2023-03-13 23:57:29 +0300

    first commit

---
+++ src/test/java/com/example/posttracker/PostTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.posttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
